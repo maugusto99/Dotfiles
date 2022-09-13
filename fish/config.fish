fe2618388a59337191936ebcaa095a661f4a054e
maugusto99 <musettiaugusto@gmail.com>
2022-09-12 22:52:37 -0300

    fish plugins fzf and color man pages

--- fish/config.fish
+++ fish/config.fish
@@ -10,29 +10,18 @@ set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin /storage/Applications /storage/Applications/vaspkit.1.3.5/bin $fish_user_paths
 
 ### EXPORT ###
-set -U fish_greeting # Supresses fish's intro message
-# set TERM "xterm-256color"                         # Sets the terminal type
-set -Ux EDITOR nvim # $EDITOR use Emacs in terminal
-set BROWSER firefox
-set TERMINAL alacritty
-set LESSHISTFILE -
+set -Ux fish_greeting # Supresses fish's intro message
+set -Ux EDITOR nvim
+set -Ux BROWSER firefox
+set -Ux TERMINAL kitty
+set -Ux LESSHISTFILE -
+set -x LS_COLORS dir_colors.sh
 
 
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 function fish_user_key_bindings
     fish_default_key_bindings
 end
-
-function fish_user_key_bindings
-    fzf_key_bindings
-end
-
-set FZF_DEFAULT_OPTS '
-    --height=50%
-    --color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1
-    --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
-    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
 
 #Aliases
 # vim and emacs
@@ -41,8 +30,8 @@ alias vdiff="nvim -d"
 
 # ls
 alias ll='exa -alh --color=auto --group-directories-first'
-alias la='exa -a --color=auto --group-directories-first'
-alias ls='exa --color=auto --group-directories-first'
+alias la='exa -a --color=auto --icons --group-directories-first'
+alias ls='exa --color=auto --icons --group-directories-first'
 
 # Colorize grep output (good for log files)
 alias grep='rg --color=auto'
@@ -59,4 +48,15 @@ alias cat="bat --theme=Nord"
 alias ag="ase gui"
 alias vesta="/storage/Applications/VESTA-gtk3/VESTA"
 alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
+
+set fzf_history_opts --with-nth=4..
+set -x FZF_DEFAULT_OPTS '
+    --height=70%
+    --color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1
+    --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
+    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
+    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b'
+
+
+# starship init fish | source
 

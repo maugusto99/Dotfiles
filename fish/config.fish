340ca6baa82e2b7409c3e287a68c38e02f49e843
maugusto99 <musettiaugusto@gmail.com>
2022-12-27 18:47:41 -0300

    change to tokyonight

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@
 # your path gets massive and fish becomes very slow.
 
 set -e fish_user_paths
-set -U fish_user_paths $HOME/.local/bin /storage/Applications/vaspkit.1.3.5/bin $XDG_CONFIG_HOME/emacs/bin $XDG_CONFIG_HOME/spicetify $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin /storage/Applications/vaspkit.1.3.5/bin $XDG_CONFIG_HOME/spicetify $fish_user_paths
 
 set XDG_CONFIG_HOME $HOME/.config
 set XDG_CACHE_HOME $HOME/.cache
@@ -13,9 +13,9 @@ set XDG_DATA_HOME $HOME/.local/share
 set -Ux fish_greeting # Supresses fish's intro message
 set -Ux EDITOR nvim
 set -Ux BROWSER firefox
-set -Ux TERMINAL konsole
+set -Ux TERMINAL alacritty
 set -Ux LESSHISTFILE -
-# (dircolors $XDG_CONFIG_HOME/fish/dir_colors)
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p --theme gruvbox-dark'"
 
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 
@@ -24,12 +24,9 @@ function fish_user_key_bindings
     fzf_key_bindings
 end
 
-# source /usr/share/fish/vendor_functions.d/fzf-key-bindings.fish
-
 #Aliases
-# vim 
+# vim
 alias vi="nvim"
-alias wezterm="org.wezfurlong.wezterm"
 
 # exa for ls
 if test -e /usr/bin/exa
@@ -59,28 +56,25 @@ alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
 set -x FZF_DEFAULT_COMMAND 'fd --strip-cwd-prefix '
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type f --no-ignore-vcs'
 set -x FZF_ALT_C_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type d --no-ignore-vcs'
-# Nord
-# set -x FZF_DEFAULT_OPTS ' 
-#   --height=70%
-#   --layout reverse
-#   --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#616E88
-#   --color=fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1
-#   --color=marker:#81A1C1,fg+:#D8DEE9,prompt:#81A1C1,hl+:#81A1C1'
-
-# Gruvbox
-set -x FZF_DEFAULT_OPTS ' 
-  --height=70%
-  --layout reverse
-  --color=bg+:#3c3836,bg:#1d2021,spinner:#8ec07c,hl:#83a598
-  --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c
-  --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598'
-
 set -x FZF_CTRL_T_OPTS "--preview ''"
 
-if test -e /usr/bin/zoxide
-  zoxide init fish | source
-end
+# Gruvbox
+# set -x FZF_DEFAULT_OPTS '
+# --height=50%
+# --layout reverse
+# --color=bg+:#3c3836,bg:#1d2021,spinner:#8ec07c,hl:#83a598
+# --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c
+# --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598'
 
-if test -e /usr/bin/starship
-  starship init fish | source
-end
+set -x FZF_DEFAULT_OPTS '
+  --height=50%
+  --layout reverse
+	--color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
+	--color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff
+	--color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
+	--color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
+
+test -e /usr/bin/zoxide && zoxide init fish | source
+
+test -e /usr/bin/starship && starship init fish | source
+

a4a84e39f10be322ff5effd077c83fc334e7c5da
maugusto99 <musettiaugusto@gmail.com>
2023-01-08 16:15:36 -0300

    first commit in fedora

--- fish/config.fish
+++ fish/config.fish
@@ -5,9 +5,6 @@
 set -e fish_user_paths
 set -x fish_user_paths $HOME/.local/bin /storage/Applications/vaspkit.1.3.5/bin $XDG_CONFIG_HOME/spicetify $fish_user_paths
 
-set XDG_CONFIG_HOME $HOME/.config
-set XDG_CACHE_HOME $HOME/.cache
-set XDG_DATA_HOME $HOME/.local/share
 
 ### EXPORT ###
 set -Ux fish_greeting # Supresses fish's intro message
@@ -19,7 +16,7 @@ set -Ux LESSHISTFILE -
 ### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
 
 function fish_user_key_bindings
-    fish_default_key_bindings
+  fish_default_key_bindings
 end
 
 
@@ -39,18 +36,17 @@ end
 alias cp="cp -i"
 alias mv='mv -i'
 alias rm='rm -i'
+alias bat='bat --theme base16'
 
-alias bat="bat --theme=base16"
-test -e /bin/bat && set -x MANPAGER "sh -c 'col -bx | bat -l man -p '"
+test -e /bin/bat && set -x MANPAGER "sh -c 'col -bx | bat -l man -p --theme base16'" && set -x MANROFFOPT "-c"
 alias ag="ase gui"
 alias wget="wget --hsts-file=$XDG_CACHE_HOME/wget-hsts"
-alias wezterm="flatpak run org.wezfurlong.wezterm"
 
 # EXPORTS
-# set -x FZF_DEFAULT_COMMAND 'fd --strip-cwd-prefix '
-# set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type f --no-ignore-vcs'
-# set -x FZF_ALT_C_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type d --no-ignore-vcs'
-# set -x FZF_CTRL_T_OPTS "--preview ''"
+set -x FZF_DEFAULT_COMMAND 'fd --strip-cwd-prefix'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type f --no-ignore-vcs'
+set -x FZF_ALT_C_COMMAND $FZF_DEFAULT_COMMAND '--hidden --type d --no-ignore-vcs'
+set -x FZF_CTRL_T_OPTS "--preview ''"
 
 set -x FZF_DEFAULT_OPTS '
 --height=70%
@@ -61,15 +57,31 @@ set -x FZF_DEFAULT_OPTS '
 --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
 --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
 
+# set -x FZF_DEFAULT_OPTS '
+# --height=50%
+# --layout reverse
+# --bind ctrl-h:toggle-preview
+# --color=bg+:#3c3836,bg:#1d2021,spinner:#8ec07c,hl:#83a598
+# --color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c
+# --color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#83a598'
+
+# set -x FZF_DEFAULT_OPTS '
+# --height=70%
+# --layout reverse
+# --bind ctrl-h:toggle-preview
+# --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+# --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
+
 # fzf.fish
 set fzf_fd_opts --hidden --exclude=.git
 set fzf_history_opts --with-nth=4..
 set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-set fzf_directory_opts --preview ""
+# set fzf_directory_opts --preview ""
 
+bind \ct  'tmux a || tmux'
+source $XDG_CONFIG_HOME/fish/themes/Tokyonight.fish
 
-bind \ct 'tmux a || tmux'
 test -e /usr/bin/zoxide && zoxide init fish | source
 
-test -e /usr/bin/starship && starship init fish | source
+test -e /usr/local/bin/starship && starship init fish | source
 
